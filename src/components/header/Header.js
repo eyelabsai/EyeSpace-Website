bcfe97962baf53cdc0077a38422b710aeb31fefa
Emily Ho <icianho@gmail.com>
2023-04-19 19:58:13 -0700

    commented out broken code

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,25 +10,21 @@ export default class Button extends React.Component {
   };
 
   buttonsList = ["Home","About Us", "Contact", "Blogs"];
-  // generates an array of div tags for the buttons
   
 
 
 
 
   render() {
-    for (let i = 1 , i <= 4 , i++) {
-
-    }
-
     return (
       <div className="navbar">
         <img src="../../assets/eyespaceLogo.svg" alt="EyeSpace Logo"/>
         <div className="buttons">
-          <div className="button1">Home</div>
-          <div className="button2">About Us</div>
-          <div className="button3">Contact</div>
-          <div className="button4">Blogs</div>
+          {/* {
+            for (let i = 0 ; i < 4 ; i++) {
+              <div
+            }
+          } */}
         </div>
       </div>
     );
