4f6136914228645677e0d48408bcd980662f7011
Emily Ho <icianho@gmail.com>
2023-04-22 13:18:35 -0700

    changed into React formatting, added comments

--- src/views/Homepage/Homepage.js
+++ src/views/Homepage/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Mission from "./Mission/Mission";
 // be sure to import the css file!
 
 export default class Homepage extends React.Component {
@@ -6,6 +7,7 @@ export default class Homepage extends React.Component {
       return (
         <div className="homepage">
           Insert your Component Tag here (under src/views/Homepage.js)
+          <Mission/>
         </div>
       );
     }
--- src/views/Homepage/Mission/Mission.js
+++ src/views/Homepage/Mission/Mission.js
@@ -1,1 +1,28 @@
 // FOR CHERRY :))
+import React from "react";
+import PropTypes from "prop-types"; 
+import './Mission.css'; // replace "TestComponent" with your own component
+
+export default class Mission extends React.Component { // replace "TestCOmponent" with your own component
+  static propTypes = { // define any props here
+    name: PropTypes.string,
+  };
+
+  render() { // whatever is returned here will be the html that is rendered when this component is being used
+    return (
+    <div className="cards">
+      {/** This is where your HTML goes. The only thing is that instead of class="" you do className="" for some reason */}
+      <div id = "title">
+        <h1>Our Mission</h1>
+      </div>
+      <div id = "content">
+        <p>Our mission is to empower ophthalmologists and surgeons with the tools they need to optimize their outcomes while providing an inclusive space to discuss topics with colleagues, mentors, and mentees</p>
+      </div>
+      <div id = "btn">
+        <button>Learn More About Us</button>
+      </div>
+    </div>
+    );
+  }
+}
+
