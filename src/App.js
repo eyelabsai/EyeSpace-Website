e9aad7b1a25c6ed159a86f2011bc258db15a3112
Emily Ho <icianho@gmail.com>
2023-04-21 23:10:53 -0700

    added routing, logo finally works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
-import Card from "./views/Homepage/Tools/Card/Card";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from "./views/Homepage/Homepage";
 //import './App.css';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Card/>
+        {/** Everything outside the BrowserRouter tag will be rendered on every single page */}
+        <BrowserRouter>
+          <Routes>
+            {/** For each path we have, add a Route tag and put the tag. */}
+            <Route path="/" element={<Homepage/>}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/views/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/header/Header";
+
+export default class Homepage extends React.Component {
+    render() {
+      return (
+        <div className="homepage">
+          <Header name="HELLO"/>
+        </div>
+      );
+    }
+  }
