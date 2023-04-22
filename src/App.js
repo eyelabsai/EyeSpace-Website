e5f6b19706775bff349845c877afdb62bec6099f
Emily Ho <icianho@gmail.com>
2023-04-22 16:06:55 -0700

    random commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from "./views/Homepage/Homepage";
+import Blog from "./views/Blog/Blog";
+import AboutUs from "./views/AboutUs/AboutUs";
+import Contact from "./views/Contact/Contact";
 //import './App.css';
 
 export default class App extends React.Component {
@@ -11,8 +14,10 @@ export default class App extends React.Component {
         <BrowserRouter>
           <Routes>
             {/** For each path we have, add a Route tag to define which element to render depending on the path. */}
-            <Route path="/" element={<Homepage/>}>
-            </Route>
+            <Route path="/" element={<Homepage/>}></Route>
+            <Route path="/AboutUs" element={ AboutUs }></Route>
+            <Route path="/Blog" element={ Blog }></Route>
+            <Route path="/Contact" element={ Contact }></Route>
           </Routes>
         </BrowserRouter>
       </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import './Header.css';
 import logo from '../../assets/eyespaceLogo.svg';
+import { Link } from 'react-router-dom';
 // REMEMBER TO IMPORT CSS FILES
+
+/**
+ * Buttons information:
+ * https://react-bootstrap.github.io/components/buttons/
+ */
 
 export default class Button extends React.Component {
   static propTypes = {
@@ -10,10 +16,11 @@ export default class Button extends React.Component {
   };
   
   f1 () {
-    console.log("page is " + this.page);
+    console.log("page is " + this.props.page);
   }
 
   buttonsList = ["Home","About Us", "Contact", "Blogs"];
+  buttonsDict = {"Home": "/", "About Us":"/AboutUs", "Contact":"/Contact", "Blogs":"/Blog"}; // maps the button names to the link to travel to
 
 
   render() {
@@ -22,7 +29,13 @@ export default class Button extends React.Component {
         <img src={logo} alt="EyeSpace Logo"/>
         <div className="buttons">
           {this.buttonsList.map((name)=> {
-            return <div className="button" style={{color: this.page === name ? 'orange' : 'black'}}>{name}</div>
+            return (
+              <Link to={{buttonsDict[name]}}>
+                <button className="button" style={{color: this.props.page === name ? '#FF6400' : '#48627E'}}>
+                  {name}
+                </button>
+              </Link>
+            )
           })}
         </div>
         <button className="login-button" onClick={this.f1}>Login/Register</button>
