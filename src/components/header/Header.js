502c3ffcd06acab6ccca5d8e010fc2eca0b94a61
Emily Ho <icianho@gmail.com>
2023-04-23 22:13:11 -0700

    set up the footer component for felicity, added comments

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import './Header.css';
-import logo from '../../assets/eyespaceLogo.svg';
+import logo from '../../assets/eyespaceLogo.svg'; // import the svg file (which you'll need to add yourself, more info on this in footer.js)
 // REMEMBER TO IMPORT CSS FILES
 
 export default class Button extends React.Component {
@@ -15,6 +15,7 @@ export default class Button extends React.Component {
   render() {
     return (
       <div className="navbar">
+        {/**The next line is how you would render the image on the website. Whichever name you use in your import statement will be put into src, and then give an alternate name for in case the picture doesn't show up correctly */}
         <img src={logo} alt="EyeSpace Logo"/>
         <div className="buttons">
           {this.buttonsList.map((name)=> {
