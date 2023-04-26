7649d13fe1ee4f49795f94f85c8dd6291af26f0a
Cherry Li <lcy@eduroam-169-233-192-9.ucsc.edu>
2023-04-25 23:09:54 -0700

    finish tools

--- src/views/Homepage/Tools/Card/Card.js
+++ src/views/Homepage/Tools/Card/Card.js
@@ -32,32 +32,32 @@ export default class Card extends React.Component {
   // For now, don't use any {} inside the return(). Anything inside {} will be treated as javascript, not html
     render() {
       return (
-        <div className = "tool">
+        <div className = "current_tools">
           <div class="title">Current Tools</div>
-          
-            <div class="cards">
-              <img class="card-img" src={Card1} alt="Card1 Logo"/>
-              <div class="detail">
-                <div class="card-title">IOL Reference</div>
-                <div class="card-content">Browse the entire spectrum of current FDA approved IOLs and find the lens right for any case using a novel search function</div>
+            <div class = "tool">
+              <div class="card">
+                <img class="card-img" src={Card1} alt="Card1 Logo"/>
+                <div class="detail">
+                  <div class="card-title">IOL Reference</div>
+                  <div class="card-content">Browse the entire spectrum of current FDA approved IOLs and find the lens right for any case using a novel search function</div>
+                </div>
+              </div>
+
+              <div class="card">
+                <img class="card-img" src={Card2} alt="Card2 Logo"/>
+                <div class="detail">
+                  <div class="card-title">Refractive Outcome Tracking</div>
+                  <div class="card-content" id = "card2">This is a state of the art outcome tracking utility that analyzes your cataract cases to learn more about refractive outcomes, surgically induced astigmatism, and personalized A-constants</div>
+                </div>
+              </div>
+
+              <div class="card">
+                <img class="card-img" src={Card3} alt="Card3 Logo"/>
+                <div class="detail">
+                  <div class="card-title">eyeSpace Forum</div>
+                  <div class="card-content">Connect with colleagues, mentors, and mentees to discuss cases, gather wisdom, and gain support</div>
               </div>
             </div>
-
-            <div class="cards">
-              <img class="card-img" src={Card2} alt="Card2 Logo"/>
-              <div class="detail">
-                <div class="card-title">Refractive Outcome Tracking</div>
-                <div class="card-content">This is a state of the art outcome tracking utility that analyzes your cataract cases to learn more about refractive outcomes, surgically induced astigmatism, and personalized A-constants</div>
-              </div>
-            </div>
-
-            <div class="cards">
-              <img class="card-img" src={Card3} alt="Card3 Logo"/>
-              <div class="detail">
-                <div class="card-title">eyeSpace Forum</div>
-                <div class="card-content">Connect with colleagues, mentors, and mentees to discuss cases, gather wisdom, and gain support"</div>
-              </div>
-          
           </div>
         </div>
       );
