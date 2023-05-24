e5e6650f13f0eda24f604479b253861b7a6601f6
fyyshih <felithecity47@gmail.com>
2023-05-23 22:46:58 -0700

    just in case

--- src/views/AboutUs/IndustryAdvisors/IndustryAdvisors.js
+++ src/views/AboutUs/IndustryAdvisors/IndustryAdvisors.js
@@ -4,7 +4,8 @@ import './IndustryAdvisors.css'; // replace "TestComponent" with your own compon
 
 import JohnsonJohnson_logo from '../../assets/JohnsonJohnsonLogo.svg';
 import Alcon_logo from '../../assets/alconLogo.svg';
-import RxSight_logo from '../../assets/RxSight.svg/';
+import RxSight_logo from '../../assets/RxSight.svg';
+import gray_rectangle from '../../assets/grayRectange.svg';
 
 
 
@@ -15,7 +16,7 @@ export default class IndustryAdvisors extends React.Component { // replace "Test
 
   render() {
     return (
-    <div className="Industry Advisors">
+    <div className="industryAdvisors">
       <div className="TitleText">
         Industry Advisors
       </div>
@@ -24,8 +25,43 @@ export default class IndustryAdvisors extends React.Component { // replace "Test
         <img src={JohnsonJohnson_logo} alt="Johnson & Johnson logo"/>
         <img src={Alcon_logo} alt="Alcon logo"/>
         <img src={RxSight_logo} alt="RxSight logo"/>
+      </div>
+
+      <div className="background"> 
+        <img src={gray_rectangle} alt="gray background rectangle"/>
 
       </div>
+
+      <div className="Advisor Names">
+        <div className="Zachary Johnson">
+          <div className="ZJ name">
+            Zachary Johnson
+          </div>
+          <div className="ZJ position">
+            Account Executive – Los Angeles
+          </div>
+        </div>
+
+        <div className="Charlie Green">
+          <div className="CG name">
+            Charlie Green
+          </div>
+          <div className="CG position">
+            Surgical Academic Manager
+          </div>
+        </div>
+
+        <div className="Paul Ericson">
+          <div className="PE name">
+            Paul Ericson
+          </div>
+          <div className="PE position">
+            Director of Sales – Mid-Central Region
+          </div>
+        </div>
+
+      </div>
+
     </div>
     );
   }
