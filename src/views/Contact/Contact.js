072d81ad5c977affcd4c258ad2f14614d0769d45
Kyle Luo <kluo15@ucsc.edu>
2023-06-28 21:29:29 -0700

    Added contact

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -13,7 +13,27 @@ export default class Contact extends React.Component {
     return (
     <div className="cards">
       <Header page="Contact"/>
-      <h1>Contact Us</h1>
+      <div className="contact-container">
+        <div className="contact-left">
+          <h3 id="lefthead">
+            Send Us A<br/>Message
+          </h3>
+          <p>
+            We’d love to hear from you! We value comments, questions, corrections, feedback, potential partnerships, and relationship with industry. Simply complete the form below and we will respond soon
+          </p>
+        </div>
+        <div className="contact-right">
+          <form action="/">
+            <p>Full Name</p>
+            <input type="text" name="fullname"></input>
+            <p>Email</p>
+            <input type="text" name="email"></input>
+            <p>Message</p>
+            <input type="text" name="email" id="message"></input>
+            <input type="submit" id="con-submission"></input>
+          </form>
+        </div>
+      </div>
       <Footer/>
     </div>
     );
