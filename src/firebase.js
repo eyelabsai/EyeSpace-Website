b0cb03b0fad7a6b47472ccb39f0829b2462595dd
Ivan-Rocks <13611041610@163.com>
2023-12-18 14:32:51 -0600

    initial firebase login

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZOk1kvNEJdi_21Mq5giHoE_KV9W0byQ0",
+  authDomain: "iol-reference.firebaseapp.com",
+  databaseURL: "https://iol-reference-default-rtdb.firebaseio.com",
+  projectId: "iol-reference",
+  storageBucket: "iol-reference.appspot.com",
+  messagingSenderId: "891421425043",
+  appId: "1:891421425043:web:81b84ccd1eccf455087c66",
+  measurementId: "G-8CHLQKQ3HB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {app, auth};
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -2,12 +2,39 @@ import React from "react";
 import PropTypes from "prop-types"; 
 import Header from '../../components/header/Header';
 import Footer from "../../components/footer/Footer";
-import './Login.css'; 
+import './Login.css';
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export default class Login extends React.Component { 
   static propTypes = { // define any props here
     name: PropTypes.string,
   };
+
+  signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, this.state.email, this.state.password)
+    .then((userCredential) => {
+      console.log(userCredential);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      email: '', 
+      password: ''
+    };
+  }
+  setEmail = (email) => {
+    this.setState({ email });
+  };
+
+  setPassword = (password) => {
+    this.setState({ password });
+  }
 
   render() {
     return (
@@ -17,17 +44,17 @@ export default class Login extends React.Component {
         <div id="email-notif"></div>
         <div className="left-login">
           <h1 id="titlelogin">Login</h1>
-          <form action="/">
+          <form onSubmit={this.signIn}>
             <label>Email</label>
             <br/>
             <div>
-              <input className="inputborder" type="text"></input>
+              <input className="inputborder" type="text" value={this.state.email} onChange={(e) => this.setEmail(e.target.value)}></input>
             </div>
             <br/>
             <label>Password</label>
             <br/>
             <div>
-              <input className="inputborder" type="password"></input>
+              <input className="inputborder" type="password" value={this.state.password} onChange={(e) => this.setPassword(e.target.value)}></input>
             </div>
             <p>
             <a href="/Login" id="forgotpw">Forgot password?</a>
