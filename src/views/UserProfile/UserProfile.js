976d4685cf21b83e4fb4b2a7ec680aa0737768e4
Ivan-Rocks <13611041610@163.com>
2023-12-21 22:08:47 -0600

    added logout button
    when logging out, user will be navigated to the index page

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -2,15 +2,31 @@ import React from "react";
 import Header from '../../components/header/Header';
 import './UserProfile.css';
 import { auth } from "../../firebase";
-import { sendPasswordResetEmail } from 'firebase/auth';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function UserProfile() {
-    
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            await auth.signOut();
+            console.log('User signed out successfully');
+            navigate('/');
+        } catch (error) {
+            // Handle any errors here
+            console.error('Error signing out: ', error);
+        }
+    }
+
     return (
-        <div >
+        <div className="userprofile">
             <Header></Header>
             <h1>User Profile</h1>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <button>Log Out</button>
+            </form>
         </div>
     )
 }
