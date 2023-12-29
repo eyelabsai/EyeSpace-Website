b0eb7cd7dd0d23d1d528124ca2b06566e94d2836
Ivan-Rocks <13611041610@163.com>
2023-12-29 17:52:48 -0600

    fixed alignment errors

--- src/views/AboutUs/DevelopersBoard/DevelopersBoard.js
+++ src/views/AboutUs/DevelopersBoard/DevelopersBoard.js
@@ -7,16 +7,16 @@ function DevelopersBoard() {
             <h1 className="developer-title">Developing Team</h1>
             <div className="developer-container">
                 <div>
-                    <h2>Machine Learning</h2>
+                    <h2 className="developer-types">Machine Learning</h2>
                     <div className="developer-names">Bhargav Panguluru</div>
                     <div className="developer-names">Ethan Babel</div>
-                    <h2>Fullstack Developers</h2>
+                    <h2 className="developer-types">Fullstack Developers</h2>
                     <div className="developer-names">Adnan Alouane</div>
                     <div className="developer-names">Ivan Zhang</div>
                     <div className="developer-names">Teresa He</div>
                 </div>
                 <div>
-                    <h2>iOS</h2>
+                    <h2 className="developer-types">iOS</h2>
                     <div className="developer-names">Rohan Song</div>
                     <div className="developer-names">Yueyang Huang</div>
                 </div>
