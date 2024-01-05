dcf94fa8d2d3024bed39b0fe72a145809b19c66c
Ivan-Rocks <13611041610@163.com>
2024-01-05 17:03:16 -0600

    can retrieve some data but requires login?

--- src/views/Exchange/Exchange.js
+++ src/views/Exchange/Exchange.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from '../../components/header/Header';
 import './Exchange.css'; 
 import ForumReplacement from '../../assets/forum_replacement.png'
 import Bg from '../../assets/blogbg.svg'
 import ForumPostCard from "./ForumPostCard/ForumPostCard";
-import './ForumPostCard/ForumPostCard.css'
+import './ForumPostCard/ForumPostCard.css';
+import { auth, firestore } from '../../firebase';
+import { doc, getDocs, collection } from 'firebase/firestore';
+import { wait } from "@testing-library/user-event/dist/utils";
 
 function Exchange(props) {
+  const [posts, setPosts] = useState(null);
+  useEffect(() => {
+    async function retrieveFirestoreData() {
+      const postsRef = collection(firestore, "posts");
+      const querySnapshot = await getDocs(postsRef);
+      setPosts(postsRef);
+    }
+    retrieveFirestoreData();
+  }, []);
+  if (posts === null) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="blog-div" style={{backgroundImage: `url(${Bg})`}}>
         <Header page="eXchange"/>
