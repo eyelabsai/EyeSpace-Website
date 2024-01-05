8ac0f9b9f7a70c32c25798290f1432d97779439e
Ivan-Rocks <13611041610@163.com>
2024-01-05 17:53:39 -0600

    connected forum to firebase

--- src/views/Exchange/Exchange.js
+++ src/views/Exchange/Exchange.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Header from '../../components/header/Header';
+import { useNavigate } from 'react-router-dom';
 import './Exchange.css'; 
 import ForumReplacement from '../../assets/forum_replacement.png'
 import Bg from '../../assets/blogbg.svg'
@@ -7,19 +8,40 @@ import ForumPostCard from "./ForumPostCard/ForumPostCard";
 import './ForumPostCard/ForumPostCard.css';
 import { auth, firestore } from '../../firebase';
 import { doc, getDocs, collection } from 'firebase/firestore';
-import { wait } from "@testing-library/user-event/dist/utils";
 
 function Exchange(props) {
   const [posts, setPosts] = useState(null);
+  const navigate = useNavigate();
+  
   useEffect(() => {
-    async function retrieveFirestoreData() {
-      const postsRef = collection(firestore, "posts");
-      const querySnapshot = await getDocs(postsRef);
-      setPosts(postsRef);
-    }
-    retrieveFirestoreData();
-  }, []);
-  if (posts === null) {
+    // Check the user's authentication status
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        // User is signed in, proceed to fetch data
+        retrieveFirestoreData();
+      } else {
+        window.alert("Please login first!");
+        navigate('/login');
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [navigate]); // Add navigate to the dependency array
+
+  async function retrieveFirestoreData() {
+    const postsRef = collection(firestore, "posts");
+    const querySnapshot = await getDocs(postsRef);
+    const postsData = querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    //console.log(postsData[0].title);
+    setPosts(postsData);
+  }
+  console.log(posts)
+
+  if (posts === null || posts === undefined) {
     return <div>Loading...</div>;
   }
   return (
@@ -32,9 +54,13 @@ function Exchange(props) {
           </div>
           <br></br><br></br><br></br><br></br>
           <div className="forum-postcard-container">
-            <ForumPostCard/>
-            <ForumPostCard/>
-            <ForumPostCard/>
+          {
+            posts.map((obj) => {
+              return (
+                <ForumPostCard title={obj.title} text={obj.text}/>
+              )
+            })
+          }
           </div>
           <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
         </div>
--- src/views/Exchange/ForumPostCard/ForumPostCard.js
+++ src/views/Exchange/ForumPostCard/ForumPostCard.js
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types"; 
+import './ForumPostCard.css';
 
-function ForumPostCard() {
+export default class FounderCardSmall extends React.Component {
+  static propTypes = { // define any props here
+    title: PropTypes.string, // title of the person (MD, PHD, etc)
+    text: PropTypes.string, // title of the person (MD, PHD, etc)
+  };
+
+  render() {
     return (
-        <div className="forum-postcard">
-            <h1>hi</h1>
-            <h1>ho</h1>
-        </div>
+    <div className="forum-postcard">
+        <div>{this.props.title}</div>
+        <div>{this.props.text}</div>
+    </div>
     );
+  }
 }
-export default ForumPostCard;
+
